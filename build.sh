6dc72a0d64e2deaaa72c962cbe9840b30331a760
Yen, David (398B-Affiliate) <david.yen@jpl.nasa.gov>
2022-04-23 09:22:11 -0700

    fix github action

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ rm -Rf venv build;mkdir venv
 
 poetry export -f requirements.txt --output requirements.txt
 pip3 install -r requirements.txt  --target ./venv
-cp .cumulus_file_rename/*.py ./venv
+cp cumulus_file_rename/*.py ./venv
 cd venv; zip -r ../artifact.zip .
